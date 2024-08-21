ccf76a4a91686f954b1716e1853f6a619d9ede8b
Luke Christopulos <lukechristopulos@gmail.com>
2024-08-21 09:01:40 -0500

    Initial Commit

---
+++ ME/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ME
+//
+//  Created by Luke R. Christopulos on 8/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ME/MEApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEApp.swift
+//  ME
+//
+//  Created by Luke R. Christopulos on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
